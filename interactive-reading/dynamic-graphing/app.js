85ec627cd5264a21b6b3532d979e88ccad4f5cb4
emirhan422 <emirhankurtulus422@gmail.com>
2020-08-11 11:26:50 +0300

    DataGraphing debugged

--- interactive-reading/dynamic-graphing/app.js
+++ interactive-reading/dynamic-graphing/app.js
@@ -1,20 +1,20 @@
 window.onload = function(){
     DataX =[0,1,2,3,4,5,6,7,8,9,10];
-    DataY = [];
-    const default_color = "rgb(87,189,130)";
+    DataY = [0,1,2,3,4,5,6,7,8,9,10];
+    const default_color = "rgb(147,147,147)";
     const red = "rgb(214,11,0)";
     var ctx = document.getElementById('myChart');
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
+            labels: DataX,
             datasets: [{
                 data: DataY,
                 borderWidth: 1,
-                label: "Y"
-            },{
-                data: DataX,
-                borderWidth: 1,
-                label: "X"
+                label: "Y",
+                fill: false,
+                borderColor:"rgb(0,0,0)",
+                backgroundColor:"rgb(0,0,0)"
             }]
         },
         options: {
@@ -33,7 +33,6 @@ window.onload = function(){
     const body = document.body;
     let input = "Placeholder text";
     convertedInput =[];
-    DataY = [1,2,3,4,5];
     myChart.update();
 
     arrow.addEventListener('click',function(event){
@@ -45,10 +44,13 @@ window.onload = function(){
         if(checkInput()){
             body.style.backgroundColor = default_color;
             for(let i = 0;i<11;i++){
-                let temp = DataX * Number(convertedInput[0]);
+                console.log(DataY[i]);
+                let temp = DataX[i] * convertedInput[0];
+                console.log("TEMP1"+temp);
                 if(convertedInput[1] == 1) temp+= Number(convertedInput[2]);
                 else temp-= Number(convertedInput[2]);
                 DataY[i] = temp;
+                console.log(DataY[i]);
             }
             myChart.update();
         }
@@ -58,12 +60,11 @@ window.onload = function(){
     function checkInput(){
         let temp = input.split("");
         let xIndex = temp.indexOf("x");
-        console.log("this is x:" + temp[xIndex + 1]);
         if(xIndex == -1){
             console.log("0");
             return false;
         } 
-        else if(temp[xIndex + 1] == '+' || temp[xIndex+1] == '-'){
+        else if(temp.indexOf("+") != -1 && temp.indexOf("-") != -1){
             console.log("1");
             console.log("X position: " + xIndex);
             return false;
@@ -72,23 +73,26 @@ window.onload = function(){
             let xDigits = "";
             let yDigits = "";
             for(let i = 0;i<xIndex;i++){
-                if(temp[i] > '0' && temp[i] <'9') xDigits = xDigits + temp[i];
+                if(temp[i] >= '0' && temp[i] <'9') xDigits = xDigits + temp[i];
                 else{
-                    console.log("2");
+                    console.log("112");
+                    console.log(temp[i]);
                     return false;
                 } 
             }
             for(let i = xIndex+2;i<temp.length;i++){
-                if(temp[i] > '0' && temp[i] <'9') yDigits = yDigits + temp[i];
+                if(temp[i] >= '0' && temp[i] <'9') yDigits = yDigits + temp[i];
                 else{
-                    console.log("2");
+                    console.log("12");
                     return false;
                 } 
             }
             let choice = -1;
             if (temp[xIndex+1] == '+') choice = 1;
             else choice = 0;
-            convertedInput.push(xDigits,choice,yDigits);
+            if(xIndex == 0) xDigits = 1;
+            convertedInput = [Number(xDigits),choice,Number(yDigits)];
+            console.log("input"+ convertedInput);
             console.log("success");
             return true;
         }
